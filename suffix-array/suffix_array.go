2a4d384d5e7e75b8109ba28b500293d8073a1af5
Mykhailo Kan <mykhailo.kan@giss.pl>
2024-04-18 12:38:50 +0200

    wron answer on test 23

--- suffix-array/suffix_array.go
+++ suffix-array/suffix_array.go
@@ -7,42 +7,94 @@ import (
 
 // https://codeforces.com/edu/course/2/lesson/2/1/practice/contest/269100/problem/A
 func Solution(s string) []int {
-	type pair struct {
-		idx    int
-		str    string
-		cleft  int
-		cright int
-	}
 	s += "$"
-	p := []pair{}
-	for i, ch := range s {
-		p = append(p, pair{
-			idx: i,
-			str: string(ch),
-		})
-	}
-	// k := 0
-	sort.Slice(p, func(i, j int) bool {
-		return byte(p[i].str[0]) < byte(p[j].str[0])
-	})
-	c := make([]int, 0, len(p))
-	class := 0
-	prev := p[0]
-	c = append(c, class)
-	for i := 1; i < len(p); i++ {
-		if prev.str != p[i].str {
-			class++
+	p := make([]int, len(s))
+	c := make([]int, len(s))
+	{
+		type pair struct {
+			idx  int
+			char byte
 		}
-		c = append(c, class)
-		prev = p[i]
+		a := make([]pair, len(s))
+		for i := 0; i < len(s); i++ {
+			a[i] = pair{
+				idx:  i,
+				char: s[i],
+			}
+		}
+		// k := 0
+		sort.Slice(a, func(i, j int) bool {
+			return a[i].char < a[j].char
+		})
+		for i := 0; i < len(s); i++ {
+			p[i] = a[i].idx
+		}
+		c[p[0]] = 0
+		for i := 1; i < len(s); i++ {
+			if a[i-1].char == a[i].char {
+				c[p[i]] = c[p[i-1]]
+			} else {
+				c[p[i]] = c[p[i-1]] + 1
+			}
+		}
 	}
-	fmt.Printf("%v\n", p)
-	fmt.Printf("%v\n", c)
-	return nil
+	k := 0
+	for 1<<k < len(s) {
+		type elem struct {
+			idx int
+			a   int
+			b   int
+		}
+		a := make([]elem, len(s))
+		for i := 0; i < len(s); i++ {
+			nextP := (p[i] + 1) % len(s)
+			a[i] = elem{
+				idx: p[i],
+				a:   c[p[i]],
+				b:   c[nextP],
+			}
+		}
+		sort.Slice(a, func(i, j int) bool {
+			if a[i].a == a[j].a {
+				if a[i].b == a[j].b {
+					return a[i].idx > a[j].idx
+				}
+				return a[i].b < a[j].b
+			} else {
+				return a[i].a < a[j].a
+			}
+
+		})
+		for i := 0; i < len(s); i++ {
+			p[i] = a[i].idx
+		}
+		c[p[0]] = 0
+		for i := 1; i < len(s); i++ {
+			if a[i-1].a == a[i].a && a[i-1].b == a[i].b {
+				c[p[i]] = c[p[i-1]]
+			} else {
+				c[p[i]] = c[p[i-1]] + 1
+			}
+		}
+		k++
+	}
+	return p
 }
 
 func main() {
 	s := ""
 	fmt.Scanf("%s", &s)
-	Solution(s)
+	a := Solution(s)
+	for i := 0; i < len(a); i++ {
+		fmt.Printf("%d ", a[i])
+	}
+	fmt.Println()
+
+	// // show with substr
+	// s += "$"
+	// for i := 0; i < len(a); i++ {
+	// 	fmt.Printf("%d\t%s", a[i], s[a[i]:])
+	// 	fmt.Println()
+	// }
+	// fmt.Println()
 }
