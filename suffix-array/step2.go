07d1ddec41f6c2b1c052d21b182c86212cd22974
Mykhailo Kan <mykhailo.kan@giss.pl>
2024-04-19 16:20:14 +0200

    with radix sort

--- suffix-array/step2.go
+++ suffix-array/step2.go
@@ -6,8 +6,8 @@ import (
 )
 
 // https://codeforces.com/edu/course/2/lesson/2/1/practice/contest/269100/problem/A
-// suffix array with radix sort
-// O(n log n)
+// suffix array with sorting
+// O(n log^2n)
 func Solution2(s string) []int {
 	s += "$"
 	p := make([]int, len(s))
@@ -49,25 +49,58 @@ func Solution2(s string) []int {
 			a   int
 			b   int
 		}
+		radixSort := func(a []elem) []elem {
+			var result []elem
+			{
+				cnt := make([]int, len(a))
+				for _, v := range a {
+					cnt[v.b]++
+				}
+				aCopy := make([]elem, len(a))
+				pos := make([]int, len(a))
+				pos[0] = 0
+				for i := 1; i < len(a); i++ {
+					pos[i] = pos[i-1] + cnt[i-1]
+				}
+				for _, v := range a {
+					i := v.b
+					aCopy[pos[i]] = v
+					pos[i]++
+				}
+				result = aCopy
+			}
+			{
+				cnt := make([]int, len(a))
+				for _, v := range a {
+					cnt[v.a]++
+				}
+				aCopy := make([]elem, len(a))
+				pos := make([]int, len(a))
+				pos[0] = 0
+				for i := 1; i < len(a); i++ {
+					pos[i] = pos[i-1] + cnt[i-1]
+				}
+				for _, v := range result {
+					i := v.a
+					aCopy[pos[i]] = v
+					pos[i]++
+				}
+				result = aCopy
+			}
+			return result
+		}
 		a := make([]elem, len(s))
 		for i := 0; i < len(s); i++ {
-			nextIdx := (p[i] + 1) % len(s)
+			nextIdx := (i + (1 << k)) % len(s)
 			a[i] = elem{
-				idx: p[i],
-				a:   c[p[i]],
+				idx: i,
+				a:   c[i],
 				b:   c[nextIdx],
 			}
 		}
-		sort.Slice(a, func(i, j int) bool {
-			if a[i].a == a[j].a {
-				if a[i].b == a[j].b {
-					return a[i].idx > a[j].idx
-				}
-				return a[i].b < a[j].b
-			} else {
-				return a[i].a < a[j].a
-			}
-		})
+		fmt.Printf("Before:\t%+v\n", a)
+		a = radixSort(a)
+		fmt.Printf("After:\t%+v\n", a)
 		for i := 0; i < len(s); i++ {
 			p[i] = a[i].idx
 		}
@@ -84,18 +117,43 @@ func Solution2(s string) []int {
 	return p
 }
 
+// simple radix sort
+// for efficiency this is a must: max(a) <= len(a) -1
+// Usage
+// arr := radixSort([]int{6, 0, 1, 5, 9, 4, 2, 3, 7, 8})
+// fmt.Printf("%v\n", arr)
+func radixSort(a []int) []int {
+	count := make([]int, len(a))
+	for _, v := range a {
+		count[v]++
+	}
+
+	pos := make([]int, len(a))
+	pos[0] = 0
+	for i := 1; i < len(a); i++ {
+		pos[i] = pos[i-1] + count[i-1]
+	}
+	aCopy := make([]int, len(a))
+	for _, v := range a {
+		aCopy[pos[v]] = v
+		pos[v]++
+	}
+	return aCopy
+}
 func Solution2Main() {
-	s := ""
-	fmt.Scanf("%s", &s)
-	a := Solution1(s)
+
+	s := "ababba"
+	// fmt.Scanf("%s", &s)
+	a := Solution2(s)
 	for i := 0; i < len(a); i++ {
 		fmt.Printf("%d ", a[i])
 	}
-	// // show with substr
-	// s += "$"
-	// for i := 0; i < len(a); i++ {
-	// 	fmt.Printf("%d\t%s", a[i], s[a[i]:])
-	// 	fmt.Println()
-	// }
-	// fmt.Println()
+	fmt.Println()
+	// show with substr
+	s += "$"
+	for i := 0; i < len(a); i++ {
+		fmt.Printf("%d\t%s", a[i], s[a[i]:])
+		fmt.Println()
+	}
+	fmt.Println()
 }
--- suffix-array/suffix_array.go
+++ suffix-array/suffix_array.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	Solution1Main()
+	Solution2Main()
 }
